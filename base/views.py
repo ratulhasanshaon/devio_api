0d17b751756f5b78c836f4aea833d231f1bb65dc
ratulhasanshaon <ratulhasanshaon@gmail.com>
2023-02-18 18:20:27 +0600

    3nd commit

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -14,16 +14,25 @@ def endpoints(request):
     return Response(data)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def user_list(request):
-    # data = ['Ratul', 'Intiser', 'Hasib']
-    query = request.GET.get('query')
-    if query == None:
-        query = ''
+    #Handles GET requests
+    if request.method == 'GET':
+        query = request.GET.get('query')
+        if query == None:
+            query = ''
 
-    users =  User.objects.filter(Q(username__icontains=query)| Q(bio__icontains=query))
-    serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
+        users =  User.objects.filter(Q(username__icontains=query)| Q(bio__icontains=query))
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        user = User.objects.create(
+            username = request.data['username'],
+            bio = request.data['bio']
+            )
+        serializer = UserSerializer(user, many=False)
+        return Response(serializer.data)
 
 @api_view(['GET'])
 def user_detail(request, username):
