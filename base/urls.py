17fdf4cb4ed159efa09aa101a37901d132ac145f
ratulhasanshaon <ratulhasanshaon@gmail.com>
2023-02-19 12:24:11 +0600

    JWT Auth added but not configured

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from . import views
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+# )
 
 
 urlpatterns = [
     path('', views.endpoints),
+
+    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
+
     path('users/', views.user_list, name='users'),
     # path('users/<str:username>/', views.user_detail)
     path('users/<str:username>/', views.UserDetails.as_view()),
--- base/views.py
+++ base/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 from django.db.models import Q
 
 from .models import User, Company
@@ -16,6 +19,7 @@ def endpoints(request):
 
 
 @api_view(['GET', 'POST'])
+# @permission_classes([IsAuthenticated])
 def user_list(request):
     #Handles GET requests
     if request.method == 'GET':
