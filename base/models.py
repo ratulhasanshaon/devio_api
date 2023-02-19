bc71978b9a358c0bc125ffdc5ec7421cc2cc6aa6
ratulhasanshaon <ratulhasanshaon@gmail.com>
2023-02-19 15:53:28 +0600

    frontend react added

--- base/models.py
+++ base/models.py
@@ -13,6 +13,8 @@ class User(models.Model):
     username = models.CharField(max_length=200)
     bio = models.TextField(max_length=250, null=True, blank=True)
     stack = models.CharField(max_length=200, null=True, blank=True)
+    profile_pic = models.ImageField(upload_to='uploads/', blank=True)
+    social_link = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return self.username
--- base/urls.py
+++ base/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 from . import views
-# from rest_framework_simplejwt.views import (
-#     TokenObtainPairView,
-#     TokenRefreshView,
-# )
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
     path('', views.endpoints),
 
-    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 
 
     path('users/', views.user_list, name='users'),
